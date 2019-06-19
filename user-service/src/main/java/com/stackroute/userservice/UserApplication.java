057464d6292eb0b2e1a6698875d756c671652005
deepashreev123 <deepashree952@gmail.com>
2019-06-19 12:17:19 +0530

    added userservice

---
+++ user-service/src/main/java/com/stackroute/userservice/UserApplication.java
@@ -0,0 +1,28 @@
+package com.stackroute.userservice;
+
+import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan(basePackages = {"com.stackroute.userservice"})
+
+
+@EnableEurekaClient
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
+
+
+
+
